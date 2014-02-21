68e11a835b850d01b4f0cfef55cfb33f1ddb0e50
stephanep <stephane.poss@alpeslasers.ch>
2014-02-21 10:27:12 +0100

    CHANGE: adapt the example to the new use case
    Change-Id: Ia27804820a1105347bf26e4da5589a44d0356134

--- Interfaces/API/Examples/TestJob.py
+++ Interfaces/API/Examples/TestJob.py
@@ -16,27 +16,9 @@ if __name__  == "__main__":
     j.setOutputSandbox("*.log")
 
     s = Sewlab()
-    sequence = ["""thickness = 22; material = AlInAs; x=0.48; mass=0.076;  gap=1.404; discont=-0.52; label="extraction"; """, 
-    """thickness = 43; material = GaInAs; x=0.47; mass=0.0427; gap=0.790; discont=0.52;""",
-    """thickness = 15; material = AlInAs; x=0.48; mass=0.076;  gap=1.404; discont=-0.52; """,
-    """thickness = 38; material = GaInAs; x=0.47; mass=0.0427; gap=0.790; discont=0.52; """,
-    """thickness = 16; material = AlInAs; x=0.48; mass=0.076;  gap=1.404; discont=-0.52; """,
-    """thickness = 34; material = GaInAs; x=0.47; mass=0.0427; gap=0.790; discont=0.52; """,
-    """thickness = 18; material = AlInAs; x=0.48; mass=0.076;  gap=1.404; discont=-0.52; """,
-    """thickness = 30; material = GaInAs; x=0.47; mass=0.0427; gap=0.790; discont=0.52; """,
-    """thickness = 21; material = AlInAs; x=0.48; mass=0.076;  gap=1.404; discont=-0.52; """,
-    """thickness = 28; material = GaInAs; x=0.47; mass=0.0427; gap=0.790; discont=0.52; doping=0.15;""",
-    """thickness = 25; material = AlInAs; x=0.48; mass=0.076;  gap=1.404; discont=-0.52; doping=0.15;""",
-    """thickness = 27; material = GaInAs; x=0.47; mass=0.0427; gap=0.790; discont=0.52; doping=0.15;""",
-    """thickness = 32; material = AlInAs; x=0.48; mass=0.076;  gap=1.404; discont=-0.52; """,
-    """thickness = 27; material = GaInAs; x=0.47; mass=0.0427; gap=0.790; discont=0.52; """,
-    """thickness = 36; material = AlInAs; x=0.48; mass=0.076;  gap=1.404; discont=-0.52; """,
-    """thickness = 25; material = GaInAs; x=0.47; mass=0.0427; gap=0.790; discont=0.52; """ ]
-    s.setSequence(sequence)
-    s.setSelfTransportOptions(["--no-superself"])
-    s.setSampleFile("sample-test.sample")
-    s.setEfield(-10.)
-    
+    s.setSteeringFile("test.xml")
+    s.setAlteredParameters("efield = -50")
+
     res = j.append(s)
     if not res["OK"]:
         gLogger.error(res["Message"])
