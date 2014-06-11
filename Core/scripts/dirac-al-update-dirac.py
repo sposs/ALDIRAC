7bc013f8681c70301217e4c998df13811dd67dec
stephanep <stephane.poss@alpeslasers.ch>
2014-06-11 09:21:52 +0200

    More log fixing
    Change-Id: I83850ff33bad7ecac08fb2a90ea2174f82c76e1d

--- Core/scripts/dirac-al-update-dirac.py
+++ Core/scripts/dirac-al-update-dirac.py
@@ -10,25 +10,25 @@ import os
 from DIRAC.Core.Utilities.Subprocess import shellCall
 import urllib2
 
-def execscript(comm):
+def execscript(log, comm):
     """
     Execute command, and catch if error
     """
     res= shellCall(0, comm)
     if not res['OK']:
-        gLogger.error("Failed call", res)
+        log.error("Failed call %s" % res['Message'])
     status  = res['Value'][0]
     
     os.unlink("tmp.sh")
     message = ''
     if status:
-        gLogger.error("Issue with call -> ", res['Value'][2])
+        log.error("Issue with call -> %s" % res['Value'][2])
         message = res['Value'][2]
     
     return (status, message)
 
 
-def fetch():
+def fetch(log):
     """
     Do a git fetch
     """
@@ -40,9 +40,9 @@ exit $?
 """)
     os.chmod("tmp.sh", 0755)
     comm = "sh -c './tmp.sh'" 
-    return execscript(comm)
+    return execscript(log, comm)
 
-def get_version(version):
+def get_version(log, version):
     """
     Checkout a version
     """
@@ -54,16 +54,16 @@ exit $?
 """ % version)
     os.chmod("tmp.sh", 0755)
     comm = "sh -c './tmp.sh'" 
-    return execscript(comm)
+    return execscript(log, comm)
 
-def getAmazonVMId( ):
+def getAmazonVMId( log ):
     """
     Get the VM ID from Amazon provider 
     """
     try:
         fd = urllib2.urlopen("http://instance-data.ec2.internal/latest/meta-data/instance-id", timeout=30)
     except urllib2.URLError:
-        gLogger.warn("Can not connect to EC2 URL. Trying address 169.254.169.254 directly...")
+        log.warn("Can not connect to EC2 URL. Trying address 169.254.169.254 directly...")
     try:
         fd = urllib2.urlopen("http://169.254.169.254/latest/meta-data/instance-id", timeout=30)
     except urllib2.URLError, e:
@@ -81,9 +81,9 @@ if __name__ == '__main__':
     l = Logger()
     l.initialize("update_dirac","/Operations/Defaults/Cloud/Logger")
 
-    res = getAmazonVMId()
+    res = getAmazonVMId(l)
     if not res['OK']:
-        l.error("Failed getting VM ID: ", res['Message'])
+        l.error("Failed getting VM ID: %s" % res['Message'])
         vmID = '0000'
     else:
         vmID = res['Value']
@@ -94,35 +94,35 @@ if __name__ == '__main__':
     ops = Operations()
     aldirac_version = ops.getValue("Cloud/ALDIRAC/Version")
     if not aldirac_version:
-        l.error("Missing ALDIRAC version to install")
+        l.error("Missing ALDIRAC version on %s" % vmID)
         dexit(1)
     l.info("Will install ALDIRAC ", aldirac_version)
     dirac_version = ops.getValue("Cloud/ALDIRAC/%s/DiracVersion" % aldirac_version)
     if not dirac_version:
-        l.error("Missing DIRAC version")
+        l.error("Missing DIRAC version on %s" % vmID)
         dexit(1)
     l.info("Will install DIRAC ", dirac_version)
     
     os.chdir(os.path.join(rootPath, "DIRAC"))
     # do git fetch
-    f_d = fetch()
+    f_d = fetch(l)
     if f_d[0]:
-        l.error("Failed:", f_d[1])
+        l.error("Failed fetching DIRAC on %s: %s" % (vmID, f_d[1]))
         dexit(1)
-    g_v_d = get_version(dirac_version)
+    g_v_d = get_version(l, dirac_version)
     if g_v_d[0]:
-        l.error("Failed updating DIRAC: ", g_v_d[1])
+        l.error("Failed updating DIRAC on %s: %s" % (vmID, g_v_d[1]))
         dexit(1)
     
     #Handle ALDIRAC
     os.chdir(os.path.join(rootPath, "ALDIRAC"))
-    f_a = fetch()
+    f_a = fetch(l)
     if f_a[0]:
-        l.error("Failed: ", f_a[1])
+        l.error("Failed fetch ALDIRAC on %s: %s" % (vmID, f_a[1]) )
         dexit(1)
-    g_v_a = get_version(aldirac_version)
+    g_v_a = get_version(l, aldirac_version)
     if g_v_a[0]:
-        l.error("Failed updating ALDIRAC: ", g_v_a[1])
+        l.error("Failed updating ALDIRAC on %s: %s" % (vmID, g_v_a[1]) )
         dexit(1)
     l.notice("All good")
     dexit(0)
