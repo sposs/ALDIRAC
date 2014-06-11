7b1d28fc3c79a5d2dcc062c41eda7e15f60acd13
stephanep <stephane.poss@alpeslasers.ch>
2014-06-11 10:27:41 +0200

    tag v1r0p0
    Change-Id: Idd63cf09450ea96309ef23dc81fe88334eabfcb5

--- __init__.py
+++ __init__.py
@@ -7,9 +7,9 @@ __path__ = extend_path(__path__, __name__)
 
 # Define Version
 
-majorVersion = 0
+majorVersion = 1
 minorVersion = 0
-patchLevel   = 31
+patchLevel   = 0
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
