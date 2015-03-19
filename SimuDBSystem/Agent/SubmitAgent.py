b9c3048e23fcf91bafbf9729d97d46bcebf75f5e
stephanep <stephane.poss@alpeslasers.ch>
2015-03-19 08:44:14 +0100

    Put the stder in stdout
    Change-Id: Ifa32db369ff27f62de1a2dfa7952c5efe07c746f

--- SimuDBSystem/Agent/SubmitAgent.py
+++ SimuDBSystem/Agent/SubmitAgent.py
@@ -166,10 +166,9 @@ class SubmitAgent(AgentModule):
         cmd = "simulase_wrapper_retrieve -v -D cldb --database_tag %s --design_id %s " \
               "--output /tmp/lastip/large_db.txt" % (database_tag, design_id)
         try:
-            res = subprocess.check_output(cmd.split())
+            res = subprocess.check_output(cmd.split(), stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as error:
             self.log.error("Bad simulase DB run:", str(error))
-            self.log.error(res)
             return S_ERROR(str(error))
         basepath = "/alpeslasers/simu/"
         final_path = os.path.join(basepath, str(simugroupid), "simulase_db.txt")
